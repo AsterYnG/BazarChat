0d6904631d9102281b57b2c69f76d2bed914171e
AsterYnG <khudyakov.03@bk.ru>
2024-05-29 18:49:59 +0300

    Small decorations with BazarChat header

--- src/main/java/com/arinc/controllers/ProfileController.java
+++ src/main/java/com/arinc/controllers/ProfileController.java
@@ -1,5 +1,6 @@
 package com.arinc.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/profile")
 public class ProfileController {
+    @PreAuthorize("isAuthenticated()")
     @GetMapping
     public String getProfilePage() {
         return "profile/profile";
